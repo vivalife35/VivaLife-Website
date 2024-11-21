e35ec20a100fe47f00339151b1071a21ca01586c
vivalife35 <vivalife35@gmail.com>
2024-11-21 11:15:39 +0300

    Added let progressHTML

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ function calculateProgress() {
     const waterProgress = ((water / waterGoal) * 100).toFixed(1);
     const stepsProgress = ((steps / stepsGoal) * 100).toFixed(1);
     const sleepProgress = ((sleep / sleepGoal) * 100).toFixed(1);
+
+    let progressHTML = `
+        <p>Water Intake: ${water}L (${waterProgress}%)</p>
+        <div class="progress-bar" style="width: ${waterProgress}%; background-color: ${waterProgress >= 100 ? 'green' : 'red'};"></div>
+        <p>Steps Walked: ${steps} (${stepsProgress}%)</p>
+        <div class="progress-bar" style="width: ${stepsProgress}%; background-color: ${stepsProgress >= 100 ? 'green' : 'red'};"></div>
+        <p>Sleep Hours: ${sleep}h (${sleepProgress}%)</p>
+        <div class="progress-bar" style="width: ${sleepProgress}%; background-color: ${sleepProgress >= 100 ? 'green' : 'red'};"></div>
+    `;
 }
